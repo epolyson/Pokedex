fb2ec06ec64a868c4e96c0f008b8f18775dcbd4e
epolyson <eikipolyson@gmail.com>
2023-08-22 07:04:32 +0900

    feat: メニューバーとグリッドの作成

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -8,16 +8,21 @@ function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [prevURL, setPrevURL] = useState("");
+  const [nextURL, setNextURL] = useState("");
   useEffect(() => {
     const fetchPokemonData = async () => {
       // すべてのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
+      // console.log(res);
       // 詳細データを取得
       loadPokemon(res.results);
+      setNextURL(res.next);
       setLoading(false);
     };
+
     fetchPokemonData();
-    console.log(pokemonData);
+    // console.log(pokemonData);
   }, []);
 
   const loadPokemon = async (data) => {
@@ -31,7 +36,28 @@ function App() {
     setPokemonData(_pokemonData);
   };
 
-  console.log(pokemonData);
+  // コンソールで見れるようにしておく
+  // console.log(pokemonData);
+
+  // 前へボタン
+  const handlePrevPage = async () => {
+    if (!prevURL) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+  // 次へボタン
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    await loadPokemon(data.results);
+    setPrevURL(data.previous);
+    setNextURL(data.next);
+    setLoading(false);
+  };
 
   return (
     <>
@@ -45,6 +71,10 @@ function App() {
               {pokemonData.map((pokemon, i) => {
                 return <Card key={i} pokemon={pokemon}></Card>;
               })}
+            </div>
+            <div className="btn">
+              <button onClick={handlePrevPage}>前へ</button>
+              <button onClick={handleNextPage}>次へ</button>
             </div>
           </>
         )}
